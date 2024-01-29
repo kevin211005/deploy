e034e7b000bc68a0e4f0cfb42a2945d5dc27a647
kevin211005 <86145579+kevin211005@users.noreply.github.com>
2024-01-28 16:19:29 -0800

    Initialized git project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from 'react';
+import {Chip, Box} from '@material-ui/core';
+import { initializeApp } from 'firebase/app';
+import {
+  getFirestore,
+  collection,
+  getDocs, 
+} from 'firebase/firestore';
 function App() {
+  const [data, setData] = useState<string[]>([]);
+  const config = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  }; 
+  const app = initializeApp(config);
+  useEffect(() => {
+    const fetchData = async () => {
+      const db = getFirestore(app);
+      const testDB = collection(db, 'TestData');
+      const snapshot = await getDocs(testDB);
+      const docsData = snapshot.docs.map(doc => doc.data());
+      const data = docsData.map((doc: any) => doc.Data);
+      setData(data);
+    }
+    fetchData();
+  }, [app]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box >
+      <h1>This is deploy test </h1>
+      {
+        data.map((item, index) => (
+          <Chip key={index} label={item} />
+        ))
+      }
+    </Box>
   );
 }
 
